250aa733a28cb8b9b64e09092000532e017bbb27
Steve Hickey <steve.hickey@freshtilledsoil.com>
2012-03-12 16:24:39 -0400

    Add validation to complex sign up form

--- javascripts/scripts.js
+++ javascripts/scripts.js
@@ -1,7 +1,32 @@
 $(document).ready(function(){
 
+  // validation rules for basic sign up form ======================== //
   $("form.basic-sign-up-form").validate({
     rules: {
+      emailAddress: {
+        required: true,
+        email: true
+      },
+      createPassword: {
+        required: true,
+        minlength: 8
+      },
+      confirmPassword: {
+        required: true,
+        equalTo: ".createPassword"
+      }
+    }
+  });
+
+  // validation rules for complex sign up form ====================== //
+  $("form.complex-sign-up-form").validate({
+    rules: {
+      fullName: {
+        required: true
+      },
+      userName: {
+        required: true
+      },
       emailAddress: {
         required: true,
         email: true
